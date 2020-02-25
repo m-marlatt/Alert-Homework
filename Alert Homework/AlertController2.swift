9d4098361fff39272cb0b43587edf275e4e269fc
Max Marlatt <m.marlatt@setonhill.edu>
2020-02-25 16:34:15 -0500

    Ready to grade

---
+++ Alert Homework/AlertController2.swift
@@ -0,0 +1,30 @@
+//
+//  AlertController2.swift
+//  Alert Homework
+//
+//  Created by Maxwell Marlatt on 2/25/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class AlertController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Alert Homework/ViewController.swift
+++ Alert Homework/ViewController.swift
@@ -5,10 +5,74 @@
 //  Created by Maxwell Marlatt on 2/25/20.
 //  Copyright © 2020 Maxwell Marlatt. All rights reserved.
 //
-
+import AudioToolbox
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var UserName: UITextField!
+    
+    //Pop Up
+    
+    @IBAction func alertPop(_ sender: Any) {
+        let alert = UIAlertController( title: "Important", message: "Royal Cola is the best soft drink.", preferredStyle: .alert)
+         
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"),
+                   style: .`default` , handler: { _ in }))
+           
+           
+               self.present(alert,animated: true, completion: nil)    }
+    
+   //Sound alert
+    
+    @IBAction func alertSound(_ sender: Any) { var soundId: SystemSoundID = 0
+               let soundFile:String=Bundle.main.path(forResource:"cop_killer", ofType: "wav")!
+               let soundURL: NSURL = NSURL (fileURLWithPath: soundFile)
+            AudioServicesCreateSystemSoundID(soundURL, &soundId)
+            AudioServicesPlaySystemSound(soundId)    }
+    
+
+    
+    @IBAction func alertLogin(_ sender: Any) {
+        let ac = UIAlertController(title: "What is your username?" , message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit" , style: .default) {[unowned ac] _ in
+        let answer = ac.textFields![0]
+        self.UserName.text = answer.text
+        
+        let alert = UIAlertController(title: "Welcome:", message: answer.text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default Action"), style:.`default`, handler:{ _ in}))
+        
+        self.present(alert,animated: true, completion: nil)}
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
