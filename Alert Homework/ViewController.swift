ea175d028c7e1a13020bb420a2f48e7dfeba70ee
Max Marlatt <m.marlatt@setonhill.edu>
2020-02-25 15:43:50 -0500

    Initial Commit

---
+++ Alert Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alert Homework
+//
+//  Created by Maxwell Marlatt on 2/25/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
